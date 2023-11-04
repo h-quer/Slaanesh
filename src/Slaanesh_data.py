96330cd139a7364847bba84792b4f87f8d1315c3
h-quer <149215387+h-quer@users.noreply.github.com>
2023-11-04 16:32:58 +0100

    Update Slaanesh_data.py

--- src/Slaanesh_data.py
+++ src/Slaanesh_data.py
@@ -84,7 +84,7 @@ def rem_pt(pt_index, gl_index):
 def add_game(name: str, igdb_id: int, platform: str, status: str, comment: str):
     global gl
     if igdb_id in gl['IGDB_ID'].values:
-        return
+        raise Exception('Game already in database')
     pt.reset_index(drop=True, inplace=True)
     gl.loc[len(gl.index)] = ["", int(igdb_id), name, platform, status,
                              dt.datetime(1900, 1, 1, 1, 1, 1, 1), int(0), int(0), int(0), "", comment]
