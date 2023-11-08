b671166d0b22cb462f9032b866bfbc349cdc500b
h-quer <149215387+h-quer@users.noreply.github.com>
2023-11-08 20:05:21 +0100

    Update Slaanesh_config.py

--- src/Slaanesh_config.py
+++ src/Slaanesh_config.py
@@ -31,11 +31,20 @@ token_timestamp = ""
 data_refresh_period = 90
 data_refresh_limit = 1
 
-show_filters = False
 row_height = 96
 dark_mode = None
 cards_width = 22
 color_coding = True
+
+display_types = ('cards', 'aggrid', 'table')
+type_playing = 'cards'
+type_played = 'aggrid'
+type_backlog = 'aggrid'
+type_wishlist = 'table'
+filter_playing = False
+filter_played = True
+filter_backlog = False
+filter_wishlist = True
 
 config = configparser.ConfigParser(allow_no_value=True)
 config.optionxform = str
@@ -58,12 +67,21 @@ def load_config():
         print('IGDB parameters not set')
         return
     if 'ui' in config:
-        global show_filters, row_height, dark_mode, cards_width, color_coding
-        show_filters = config.getboolean('ui', 'show_filters', fallback=show_filters)
+        global row_height, dark_mode, cards_width, color_coding
         row_height = config.getint('ui', 'row_height', fallback=row_height)
         dark_mode = config.getboolean('ui', 'dark_mode', fallback=dark_mode)
         cards_width = config.getint('ui', 'cards_width', fallback=cards_width)
         color_coding = config.getboolean('ui', 'color_coding', fallback=color_coding)
+    if 'tabs' in config:
+        global type_playing, type_played, type_backlog, type_wishlist, filter_playing, filter_played, filter_backlog, filter_wishlist
+        type_playing = config.get('tabs', 'type_playing', fallback=type_playing)
+        type_played = config.get('tabs', 'type_played', fallback=type_played)
+        type_backlog = config.get('tabs', 'type_backlog', fallback=type_backlog)
+        type_wishlist = config.get('tabs', 'type_wishlist', fallback=type_wishlist)
+        filter_playing = config.getboolean('tabs', 'filter_playing', fallback=filter_playing)
+        filter_played = config.getboolean('tabs', 'filter_played', fallback=filter_played)
+        filter_backlog = config.getboolean('tabs', 'filter_backlog', fallback=filter_backlog)
+        filter_wishlist = config.getboolean('tabs', 'filter_wishlist', fallback=filter_wishlist)
     if 'platforms' in config:
         global platform_list
         platform_list.clear()
@@ -98,35 +116,108 @@ def load_config():
     status_list_unplayed = status_list_backlog + status_list_wishlist + status_list_playing
 
 
-def update_config(access_token=None, expiry_timestamp=None,
-                  new_platform_list=None, new_backlog=None, new_wishlist=None, new_playing=None, new_played_pos=None, new_played_neg=None):
-    if access_token:
-        config['igdb']['auth_token'] = access_token
-    if expiry_timestamp:
-        config['igdb']['token_timestamp'] = expiry_timestamp
+def update_config(new_access_token=None, new_expiry_timestamp=None,
+                  new_platform_list=None, new_backlog=None, new_wishlist=None, new_playing=None, new_played_pos=None, new_played_neg=None,
+                  new_row_height=None, new_dark_mode=None, new_cards_width=None, new_color_coding=None,
+                  new_type_playing=None, new_type_played=None, new_type_backlog=None, new_type_wishlist=None,
+                  new_filter_playing=None, new_filter_played=None, new_filter_backlog=None, new_filter_wishlist=None):
+    if new_access_token:
+        global auth_token
+        config['igdb']['auth_token'] = auth_token = new_access_token
+    if new_expiry_timestamp:
+        global token_timestamp
+        config['igdb']['token_timestamp'] = token_timestamp = new_expiry_timestamp
+
     if new_platform_list:
+        if 'platforms' not in config:
+            config.add_section('platforms')
+        global platform_list
+        platform_list = new_platform_list.split(sep=',')
         config['platforms'].clear()
-        for item in new_platform_list.split(sep=','):
+        for item in platform_list:
             config.set('platforms', item)
     if new_playing:
+        if 'playing' not in config:
+            config.add_section('playing')
+        global status_list_playing
+        status_list_playing = new_playing.split(sep=',')
         config['playing'].clear()
-        for item in new_playing.split(sep=','):
+        for item in status_list_playing:
             config.set('playing', item)
     if new_backlog:
+        if 'backlog' not in config:
+            config.add_section('backlog')
+        global status_list_backlog
+        status_list_backlog = new_backlog.split(sep=',')
         config['backlog'].clear()
-        for item in new_backlog.split(sep=','):
+        for item in status_list_backlog:
             config.set('backlog', item)
     if new_wishlist:
+        if 'wishlist' not in config:
+            config.add_section('wishlist')
+        global status_list_wishlist
+        status_list_wishlist = new_wishlist.split(sep=',')
         config['wishlist'].clear()
-        for item in new_wishlist.split(sep=','):
+        for item in status_list_wishlist:
             config.set('wishlist', item)
     if new_played_pos:
+        if 'played positive' not in config:
+            config.add_section('played positive')
+        global status_list_played_pos
+        status_list_played_pos = new_played_pos.split(sep=',')
         config['played positive'].clear()
-        for item in new_played_pos.split(sep=','):
+        for item in status_list_played_pos:
             config.set('played positive', item)
     if new_played_neg:
+        if 'played negative' not in config:
+            config.add_section('played negative')
+        global status_list_played_neg
+        status_list_played_neg = new_played_neg.split(sep=',')
         config['played negative'].clear()
-        for item in new_played_neg.split(sep=','):
+        for item in status_list_played_neg:
             config.set('played negative', item)
+
+    if 'ui' not in config:
+        config.add_section('ui')
+    if new_row_height:
+        global row_height
+        config['ui']['row_height'] = row_height = new_row_height
+    if new_dark_mode:
+        global dark_mode
+        config['ui']['dark_mode'] = dark_mode = new_dark_mode
+    if new_cards_width:
+        global cards_width
+        config['ui']['cards_width'] = cards_width = new_cards_width
+    if new_color_coding:
+        global color_coding
+        config['ui']['color_coding'] = color_coding = new_color_coding
+
+    if 'tabs' not in config:
+        config.add_section('tabs')
+    if new_type_playing:
+        global type_playing
+        config['tabs']['type_playing'] = type_playing = new_type_playing
+    if new_type_played:
+        global type_played
+        config['tabs']['type_played'] = type_played = new_type_played
+    if new_type_backlog:
+        global type_backlog
+        config['tabs']['type_backlog'] = type_backlog = new_type_backlog
+    if new_type_wishlist:
+        global type_wishlist
+        config['tabs']['type_wishlist'] = type_wishlist = new_type_wishlist
+    if new_filter_playing:
+        global filter_playing
+        config['tabs']['filter_playing'] = filter_playing = new_filter_playing
+    if new_filter_played:
+        global filter_played
+        config['tabs']['filter_played'] = filter_played = new_filter_played
+    if new_filter_backlog:
+        global filter_backlog
+        config['tabs']['filter_backlog'] = filter_backlog = new_filter_backlog
+    if new_filter_wishlist:
+        global filter_wishlist
+        config['tabs']['filter_wishlist'] = filter_wishlist = new_filter_wishlist
+
     with open(file_config, 'w') as configfile:
         config.write(configfile)
